530a240c5b4dabd115737e6ee4cfa48922ad34d5
karina BRANTE <karina.brante@gmail.com>
2017-03-06 17:15:12 +0100

    ca marche mais bon...

--- static/js/script.js
+++ static/js/script.js
@@ -1,28 +1,26 @@
 $(document).ready(function() {
-    $("input").blur(function(){
+    $("input").blur(function() {
         var char = $(this).val().length;
-        if (char <= 5) {
+        if (char <= 5 || char > 30) {
             $(this).parent().addClass('has-error')
-        }
-        else {
+        } else {
             $(this).parent().removeClass('has-error').addClass('has-success')
-            $(this).parent().append("<span class='glyphicon glyphicon-ok form-control-feedback' aria-hidden='true'></span>")
             $("label").addClass('control-label')
         }
     })
     $("#password2").blur(function() {
         if ($("#password").val() === $("#password2").val()) {
             $("#password, #password2").parent().addClass('has-success')
-            
-        }
-        else {
+
+        } else {
             $("#password, #password2").parent().addClass('has-error')
 
         }
     });
-    $("#submit").click(function(){
+    $("#submit").click(function() {
         if ($("input").val().length == 0) {
             alert("veuillez saisir les champs")
         }
     })
+
 });
