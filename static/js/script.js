2f1caa1dd9db455108135d2356f5fb7fe65e1d4d
karina BRANTE <karina.brante@gmail.com>
2017-03-07 09:48:14 +0100

    plugins yarhozrh
    git commit -m plugin
    
    git commit -m plugins

--- static/js/script.js
+++ static/js/script.js
@@ -1,26 +1,27 @@
 $(document).ready(function() {
-    $("input").blur(function() {
-        var char = $(this).val().length;
-        if (char <= 5 || char > 30) {
-            $(this).parent().addClass('has-error')
-        } else {
-            $(this).parent().removeClass('has-error').addClass('has-success')
-            $("label").addClass('control-label')
-        }
-    })
-    $("#password2").blur(function() {
-        if ($("#password").val() === $("#password2").val()) {
-            $("#password, #password2").parent().addClass('has-success')
-
-        } else {
-            $("#password, #password2").parent().addClass('has-error')
-
-        }
-    });
-    $("#submit").click(function() {
-        if ($("input").val().length == 0) {
-            alert("veuillez saisir les champs")
-        }
-    })
-
+    // $("input").blur(function() {
+    //     var char = $(this).val().length;
+    //     if (char <= 5 || char > 30) {
+    //         $(this).parent().addClass('has-error')
+    //     } else {
+    //         $(this).parent().removeClass('has-error').addClass('has-success')
+    //         $("label").addClass('control-label')
+    //     }
+    // })
+    // $("#password2").blur(function() {
+    //     if ($("#password").val() === $("#password2").val()) {
+    //         $("#password, #password2").parent().addClass('has-success')
+    //
+    //     } else {
+    //         $("#password, #password2").parent().addClass('has-error')
+    //
+    //     }
+    // });
+    // $("#submit").click(function() {
+    //     if ($("input").val().length == 0) {
+    //         alert("veuillez saisir les champs")
+    //     }
+    // })
+    $('#myForm').validator()
+    $(':password').pwstrength();
 });
